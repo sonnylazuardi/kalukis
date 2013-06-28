77372bb285a0116385075220bdad1c1f10848002
A. Akbar Hidayat <keripix@gmail.com>
2013-06-28 17:37:40 +0700

    simplify build

--- build.js
+++ build.js
@@ -1,42 +1,10 @@
 ({
   baseUrl: "apps",
-  paths:{
-    requireLib:"../components/requirejs/require",
-    fabric: "../libs/fabric",
-    jquery: "../components/jquery/jquery",
-    hbs: "../components/require-handlebars-plugin/hbs",
-    handlebars: "../components/require-handlebars-plugin/Handlebars",
-    underscore: "../components/require-handlebars-plugin/hbs/underscore",
-    json2: "../components/require-handlebars-plugin/hbs/json2",
-    i18nprecompile: "../components/require-handlebars-plugin/hbs/i18nprecompile",
-    bootstrap: "../components/bootstrap/js/bootstrap",
-    spectrum: "../components/spectrum/spectrum"
-  },
-  shim:{
-    fabric: {
-      exports: "fabric"
-    },
-    jquery: {
-      exports: "$"
-    },
-    "spectrum": ["jquery"],
-    "../components/flight/lib/component": {
-      deps: ["jquery"]
-    },
-    bootstrap: {
-      deps: ["jquery"]
-    }
-  },
-  hbs: {
-    disableI18n: true
-  },
-  map: {
-    "*": {
-      'flight/component': '../components/flight/lib/component',
-      'brushes': 'brushes'
-    }
+  paths: {
+    requireLib: "../components/requirejs/require"
   },
   name: "../main",
+  mainConfigFile: "main.js",
   include: ["requireLib"],
   out: "main-built.js"
 })
