00c6c3a0c05e9c65bf3a6d84b110498ada41c059
A. Akbar Hidayat <keripix@gmail.com>
2013-06-30 23:12:46 +0700

    build script

---
+++ build/scripts/data/with_canvas.js
@@ -0,0 +1,24 @@
+/**
+ * Provides a mixin for getting the instance of canvas.
+ */
+define(function(){
+
+  return withCanvas;
+
+  function withCanvas(){
+    this.defaultAttrs({
+      canvas: ""
+    });
+
+    this.after("initialize", function(){
+      this.on(document, "canvasReady", this.setCanvas);
+
+      // I want to have a reference to the canvas
+      this.trigger(document, "canvasRequested");
+    });
+
+    this.setCanvas = function(e, eObj){
+      this.attr.canvas = eObj.canvas;
+    };
+  }
+});
---
+++ build/scripts/ui/colorpicker.js
@@ -0,0 +1,34 @@
+/**
+ * This component manages the widget for colorpicker. After
+ * initialization, it will add the colorpicker widget to the
+ * dom that this component is attached to.
+ */
+define(function(require){
+  var defineComponent = require("flight/component"),
+      withCanvas = require("data/with_canvas"),
+      tmpl = require("hbs!templates/colorpicker");
+
+  return defineComponent(colorPicker, withCanvas);
+
+  function colorPicker(){
+    this.after("initialize", function(){
+      this.$node.append(tmpl);
+
+      var cp = this.$node.children("#color-picker"),
+          me = this;
+
+      // settings
+      cp.spectrum({
+        clickoutFiresChange: true,
+        showButtons: false,
+        // fire me on color changed
+        change: function(color){
+          me.trigger(document, "colorChanged", {
+            key: "color",
+            color: color.toHexString()
+          });
+        }
+      });
+    });
+  }
+});
