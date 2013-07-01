3e4376ec855d9167b0f171ec9fc12deaf3620f40
A. Akbar Hidayat <keripix@gmail.com>
2013-07-01 09:57:05 +0700

    update build script

--- build/scripts/app.build.js
+++ build/scripts/app.build.js
@@ -5,11 +5,9 @@
   name: "main",
   mainConfigFile: "main.js",
   paths: {
-    requireLib: "vendor/requirejs/require",
-    "es5-sham": "vendor/es5-shim/es5-sham",
-    "es5-shim": "vendor/es5-shim/es5-shim"
+    requireLib: "vendor/requirejs/require"
   },
-  include: ["requireLib", "es5-sham", "es5-shim"],
+  include: ["requireLib"],
   optimize: "uglify2",
   skipDirOptimize: true,
   optimizeCss: "standard"
