40b64544fdccf8c0a65224e19131afb50003ecfb
A. Akbar Hidayat <keripix@gmail.com>
2013-07-17 18:12:44 +0700

    filling up with_paint_shape mixin

--- apps/scripts/ui/with_paint_shape.js
+++ apps/scripts/ui/with_paint_shape.js
@@ -1,8 +1,17 @@
 /**
  * A mixin for painting a certain shape.
+ *
+ * To use this mixin, the user has to have these methods:
+ *
+ * getOutlinePainter()
+ * afterFinishCallback()
+ *
  * @return {[type]} [description]
  */
-define(function(){
+define(function(require){
+
+  var compose = require("flight/lib/compose"),
+      advice = require("flight/lib/advice");
 
   function withPaintShape(){
     this.defaultAttrs({
@@ -17,14 +26,40 @@ define(function(){
       this.on(document, "uiBrushClicked", this.onUiBrushClicked);
     });
 
+    this.onClick = function(e, eObj){
+      this.attr.isPainting = true;
+
+      this.trigger(document, "uiBrushClicked", {clicked: "line"});
+
+      this.on(document, "selectedBrushReady", this.onSelectedBrushReady);
+
+      this.attr.outlinePainter = this.getOutlinePainter();
+
+      compose.mixin(this.attr.outlinePainter, [advice.withAdvice]);
+
+      this.attr.outlinePainter.after("finish", function(){
+        this.afterFinishCallback();
+      }.bind(this));
+
+      this.trigger(document, "selectedBrushRequested");
+    };
+
     this.onUiBrushClicked = function(e, eObj){
-      if (eObj.clicked !== this.attr.type){
+      if (eObj.clicked !== this.attr.type && this.attr.isPainting){
         this.trigger(document, "paintStopRequested");
       }
     };
 
     this.setBrushProperty = function(e, eObj){
+      this.attr.brush[eObj.key] = eObj[eObj.key];
+    };
 
+    this.setBrush = function(e, eObj){
+      this.attr.brushId = eObj.selectedId;
+    };
+
+    this.onSelectedBrushReady = function(e, eObj){
+      this.setBrush(e, eObj);
     };
   }
 
