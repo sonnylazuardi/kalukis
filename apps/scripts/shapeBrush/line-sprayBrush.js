0a3c802a766d102965f98f1828a1840e85569c23
A. Akbar Hidayat <keripix@gmail.com>
2013-07-17 16:46:32 +0700

    draw spray brush on line -- WARNING slow

--- apps/scripts/shapeBrush/line-sprayBrush.js
+++ apps/scripts/shapeBrush/line-sprayBrush.js
@@ -1,3 +1,28 @@
 define(function(require){
+  var sprayBrush = require("brushes/sprayBrush"),
+      lineOutliner = require("utils/lineOutline");
 
+  function createLineCircleBrush(canvas, cfg){
+    var sb = sprayBrush.create(canvas),
+        outline = lineOutliner(sb, cfg.x1, cfg.y1, cfg.x2, cfg.y2),
+        outlineLength = outline.length;
+
+    sb.color = cfg.color || "#000000";
+
+    for (var i = 0; i < outlineLength; i++){
+      sb.addSprayChunk(outline[i]);
+    }
+
+    sb.onMouseUp();
+  }
+
+  return {
+    create: function(canvas, cfg){
+      if (!cfg.x1 || !cfg.x2 || !cfg.y1 || !cfg.y2){
+        throw new Error("Required params not provided");
+      }
+
+      return createLineCircleBrush(canvas, cfg);
+    }
+  };
 });
