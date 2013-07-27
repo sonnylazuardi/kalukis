22100e97f473b8a7abb3db6358dcbcf485f3af5e
A. Akbar Hidayat <keripix@gmail.com>
2013-07-27 12:43:49 +0700

    passing the test'

--- tests/main.js
+++ tests/main.js
@@ -4,15 +4,12 @@ require.config({
   paths: {
     fabric: "canvas-lib/fabric",
     text: "vendor/requirejs-text/text",
-    handlebars: "vendor/handlebars/handlebars"
+    mustache: "vendor/mustache/mustache"
   },
 
   shim:{
     fabric: {
       exports: "fabric"
-    },
-    'handlebars': {
-      exports: 'Handlebars'
     }
   },
 
