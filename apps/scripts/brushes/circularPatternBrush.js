c4a1aa9cc588ee84c5e7a2f4f04d390f554f9a5a
A. Akbar Hidayat <keripix@gmail.com>
2013-07-29 03:34:47 +0700

    add outline points to brush using _points property

--- apps/scripts/brushes/circularPatternBrush.js
+++ apps/scripts/brushes/circularPatternBrush.js
@@ -29,7 +29,7 @@ define(function(require){
       var outline = this.createOutline(b, cfg.shape, cfg);
 
       for (var i = outline.length - 1; i >= 0; i--) {
-        b._captureDrawingPath(outline[i]);
+        b._points.push(new fabric.Point(outline[i].x, outline[i].y));
       }
 
       b._finalizeAndAddPath();
