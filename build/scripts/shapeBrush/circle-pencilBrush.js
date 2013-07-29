c5ee0504d064e42502c0775957ac3df611ff9924
A. Akbar Hidayat <keripix@gmail.com>
2013-07-29 12:37:39 +0700

    build version

--- build/scripts/shapeBrush/circle-pencilBrush.js
+++
@@ -1,1 +0,0 @@
-define(["require","fabric"],function(t){var e=t("fabric");return{create:function(t,i){if(!i.x||!i.y||!i.radius)throw new Error("Required params not provided");var n=new e.Circle({left:i.x,top:i.y,radius:i.radius,fill:null,stroke:i.color,strokeWidth:i.brushWidth||1});t.add(n).renderAll()}}});
