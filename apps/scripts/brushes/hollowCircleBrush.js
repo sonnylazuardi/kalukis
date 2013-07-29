e2051f6a080ed2bf742c4036f12ad175556db2c9
A. Akbar Hidayat <keripix@gmail.com>
2013-07-29 14:27:20 +0700

    hollow circles for shaped object painting

--- apps/scripts/brushes/hollowCircleBrush.js
+++ apps/scripts/brushes/hollowCircleBrush.js
@@ -1,14 +1,35 @@
 define(function(require){
-  var fabric = require("fabric");
+  var fabric = require("fabric"),
+      rectOutlinePts = require("utils/rectOutlinePoints"),
+      circleOutlinePts = require("utils/circleOutlinePoints"),
+      lineOutlinePts = require("utils/lineOutlinePoints"),
+      circleBrushHelper = require("brushes/circleBrushHelper");
 
   // extend fabric.CircleBrush
   var HollowCircle = fabric.util.createClass(fabric.CircleBrush, {
+    // we need custom method to add Points, because each points shouldn't
+    // have fill color, but stroke color
+    addPoint: function(pointer){
+      var pointerPoint = new fabric.Point(pointer.x, pointer.y);
+
+      var circleRadius = fabric.util.getRandomInt(
+                          Math.max(0, this.width - 20), this.width + 20) / 2;
+
+      var strokeColor = new fabric.Color(this.color)
+                          .setAlpha(fabric.util.getRandomInt(0, 100) / 100)
+                          .toRgba();
+
+      pointerPoint.radius = circleRadius;
+      pointerPoint.strokeColor = strokeColor;
+
+      this.points.push(pointerPoint);
+
+      return pointerPoint;
+    },
+
     onMouseMove: function(pointer){
       var point = this.addPoint(pointer);
       var ctx = this.canvas.contextTop;
-      // change color
-      point.strokeColor = point.fill;
-      point.fill = null;
 
       ctx.lineWidth = 1;
       ctx.strokeStyle = point.strokeColor;
@@ -48,11 +69,35 @@ define(function(require){
     },
 
     createOutline: function(brush, shape, cfg){
+      // TODO can we simplify this?
+      // TODO parameter checking
+      if (shape === "rect") {
+        return rectOutlinePts(brush, cfg.x, cfg.y, cfg.width, cfg.height);
+      } else if (shape === "circle") {
+        return circleOutlinePts(brush, cfg.x, cfg.y, cfg.radius);
+      } else if (shape === "line") {
+        return lineOutlinePts(brush, cfg.x1, cfg.y1, cfg.x2, cfg.y2);
+      }
 
+      return;
     },
 
     createShapeBrush: function(canvas, cfg){
+      var b = this.create(canvas);
+      b.width = cfg.brushWidth || 10;
 
+      var outline = this.createOutline(b, cfg.shape, cfg),
+          outlineLength = outline.length;
+
+      b.color = cfg.color || "#000000";
+
+      for (var i = 0; i < outlineLength; i++){
+        b.addPoint(outline[i]);
+      }
+
+      circleBrushHelper.drawCircles(canvas, {
+        points: b.points
+      });
     }
   };
 });
