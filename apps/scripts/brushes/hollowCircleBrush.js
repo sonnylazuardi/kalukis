d9d728659a13b904d9a2eb4e88b77da8e0dcd286
A. Akbar Hidayat <keripix@gmail.com>
2013-07-30 22:49:55 +0700

    migrate hollowCircleBrush to use with_outline_helper mixin

--- apps/scripts/brushes/hollowCircleBrush.js
+++ apps/scripts/brushes/hollowCircleBrush.js
@@ -1,9 +1,8 @@
 define(function(require){
   var fabric = require("fabric"),
-      rectOutlinePts = require("utils/rectOutlinePoints"),
-      circleOutlinePts = require("utils/circleOutlinePoints"),
-      lineOutlinePts = require("utils/lineOutlinePoints"),
-      circleBrushHelper = require("brushes/circleBrushHelper");
+      circleBrushHelper = require("brushes/circleBrushHelper"),
+      compose = require("flight/lib/compose"),
+      withOutlineHelper = require("brushes/with_outline_helper");
 
   // extend fabric.CircleBrush
   var HollowCircle = fabric.util.createClass(fabric.CircleBrush, {
@@ -63,23 +62,9 @@ define(function(require){
     }
   });
 
-  return {
+  var hollowCircleBrush =  {
     create: function(canvas){
       return new HollowCircle(canvas);
-    },
-
-    createOutline: function(brush, shape, cfg){
-      // TODO can we simplify this?
-      // TODO parameter checking
-      if (shape === "rect") {
-        return rectOutlinePts(brush, cfg.x, cfg.y, cfg.width, cfg.height);
-      } else if (shape === "circle") {
-        return circleOutlinePts(brush, cfg.x, cfg.y, cfg.radius);
-      } else if (shape === "line") {
-        return lineOutlinePts(brush, cfg.x1, cfg.y1, cfg.x2, cfg.y2);
-      }
-
-      return;
     },
 
     createShapeBrush: function(canvas, cfg){
@@ -100,4 +85,8 @@ define(function(require){
       });
     }
   };
+
+  compose.mixin(hollowCircleBrush, [withOutlineHelper]);
+
+  return hollowCircleBrush;
 });
