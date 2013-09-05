cd041e14e179a372b449aa761f0fb6facfbd38c2
A. Akbar Hidayat <keripix@gmail.com>
2013-09-05 14:14:35 +0700

    event listener called

--- tests/specs/painters/withBrushPainterSpec.js
+++ tests/specs/painters/withBrushPainterSpec.js
@@ -6,6 +6,18 @@ describeMixin("painters/withBrushPainter", function(){
 
   describe("Brush Property Events", function(){
 
+    it("Should have executed listeners for brushPropertyUpdated", function(){
+      spyOn(this.component, "updateBrushProperty");
+
+      $('.component-root').trigger("brushPropertyUpdated", {
+        key: "fillColor",
+        oldValue: "green",
+        newValue: "red"
+      });
+      expect(this.component.updateBrushProperty).toHaveBeenCalled;
+
+    });
+
   });
 
   describe("Brush Painting Flow", function(){
