7904b4f85835b7116d0cb70c16d7690535a1a604
A. Akbar Hidayat <keripix@gmail.com>
2013-09-05 14:24:55 +0700

    documentation

--- apps/src/painters/withBrushPainter.js
+++ apps/src/painters/withBrushPainter.js
@@ -23,16 +23,34 @@ define(function(require){
     this.after("initialize", function(){
       this.on("brushPaintingInitted", this.prepareBrushPainting);
       this.on("brushPropertyUpdated", this.updateBrushProperty);
+      this.on("activeBrushUpdated", this.setActiveBrush);
     });
 
+    /**
+     * Prepare steps for painting
+     */
     this.prepareBrushPainting = function(e, data){
-      
+
     };
 
+    /**
+     * Update brush property
+     * @param  {String} e    Event
+     * @param  {Object} data Event Data
+     */
     this.updateBrushProperty = function(e, data){
       if (data.key && data.newValue) {
         this.attr.prop[data.key] = data.newValue;
       }
+    };
+
+    /**
+     * Set active brush
+     * @param {String} e    Event
+     * @param {Object} data Event Data
+     */
+    this.setActiveBrush = function(e, data){
+
     };
 
   }
