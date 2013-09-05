9e752cc8c603ce28ebbe7f7ddcddf1ed37055dae
A. Akbar Hidayat <keripix@gmail.com>
2013-09-05 19:17:25 +0700

    app.js structure

--- apps/src/app.js
+++ apps/src/app.js
@@ -7,7 +7,18 @@ define(function(require){
       brushManager = require("brushManager");
 
   function boots(){
+    /**
+     * Any components that needs to hold a reference
+     * to canvas instance and canvas element, should be
+     * placed in this upper block, before the `lukis`
+     * component is instantiated
+     */
     brushManager.attachTo(document);
+
+    /**
+     * The lukis component will publish the canvas instance
+     * and its DOM element with an event name of `canvasConstructed`.
+     */
     lukis.attachTo(document, {
       canvasEl: "#lukis"
     });
