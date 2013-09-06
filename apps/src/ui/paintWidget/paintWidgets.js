d73a1e337da6877ef6b6bab37398687dc53246c9
A. Akbar Hidayat <keripix@gmail.com>
2013-09-06 18:14:50 +0700

    component's draft

--- apps/src/ui/paintWidget/paintWidgets.js
+++ apps/src/ui/paintWidget/paintWidgets.js
@@ -1,10 +1,30 @@
 define(function(require){
 
-  var defineComponent = require("flight/lib/component");
+  var defineComponent = require("flight/lib/component"),
+      mustache = require("mustache"),
+      tmpl = require("text!ui/paintWidget/template.html");
 
   return defineComponent(paintWidgets);
 
   function paintWidgets(){
+
+    this.after("initialize", function(){
+      this.attachEventListeners();
+    });
+
+    this.attachEventListeners = function(){
+      this.on("click", this.publishClickedPaintWidget);
+
+      this.on("brushlistUpdated", this.renderTemplate);
+    };
+
+    this.publishClickedPaintWidget = function(e, data){
+      this.trigger(document, "paintWidgetClicked");
+    };
+
+    this.renderTemplate = function(e, data){
+
+    };
     
   }
 
