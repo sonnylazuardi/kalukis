d4754bd1541821585d6c5cdde0cade260f3c4f48
A. Akbar Hidayat <keripix@gmail.com>
2013-09-07 05:29:19 +0700

    tested

--- tests/specs/painters/withOutlinePainterSpec.js
+++ tests/specs/painters/withOutlinePainterSpec.js
@@ -9,6 +9,18 @@ define(function(require){
       setupComponent();
     });
 
+    describe("Event Listener", function(){
+
+      it("Should have changed the active outline shape on activeOutlineShapeUpdated event", function(){
+        $('.component-root').trigger("activeOutlineShapeUpdated", {
+          newActiveOutlineShape: "example"
+        });
+
+        expect(this.component.attr.activeOutlineShape).toEqual("example");
+      });
+
+    });
+
   });
 
 });
