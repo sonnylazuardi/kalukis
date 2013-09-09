355b6d889a6fe541f157e78f473de89667ab662f
A. Akbar Hidayat <keripix@gmail.com>
2013-09-09 20:04:34 +0700

    documentation

--- apps/src/ui/paintWidget/paintWidgets.js
+++ apps/src/ui/paintWidget/paintWidgets.js
@@ -1,3 +1,7 @@
+/**
+ * This module is responsible for rendering paint widget list on
+ * the template
+ */
 define(function(require){
 
   var defineComponent = require("flight/lib/component"),
@@ -12,12 +16,18 @@ define(function(require){
       this.attachEventListeners();
     });
 
+    /**
+     * Listen to events
+     */
     this.attachEventListeners = function(){
       this.on("click", this.publishClickedPaintWidget);
 
       this.on(document, "paintWidgetsLoaded", this.renderTemplate);
     };
 
+    /**
+     * We need to publish paint widget clicked event
+     */
     this.publishClickedPaintWidget = function(e){
       if (e.target.id) {
         this.trigger(document, "paintWidgetClicked", {
@@ -26,6 +36,11 @@ define(function(require){
       }
     };
 
+    /**
+     * Render the list of widgets
+     * @param  {String} e    Event
+     * @param  {Object} data Event Data
+     */
     this.renderTemplate = function(e, data){
       var widgetList = mustache.render(tmpl, data);
       if (this.$node.children().length) {
