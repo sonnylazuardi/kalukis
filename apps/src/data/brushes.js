2767a95b24e4d143c8909885e3a73c61a72f5cfd
A. Akbar Hidayat <keripix@gmail.com>
2013-09-09 14:13:51 +0700

    put data on a json file

---
+++ apps/src/data/brushes.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": "pencil",
+    "name": "Pencil"
+  },
+  {
+    "id": "spray",
+    "name": "Spray"
+  },
+  {
+    "id": "circle",
+    "name": "Circle"
+  },
+  {
+    "id": "hollowCircle",
+    "name": "Hollow Circle"
+  },
+  {
+    "id": "hollowSquare",
+    "name": "Hollow Square"
+  },
+  {
+    "id": "circularPattern",
+    "name": "Circular Pattern"
+  }
+]
