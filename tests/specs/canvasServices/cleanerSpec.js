297ebfdbce6d2de4cda8fb1af1026e83bc30ed5b
A. Akbar Hidayat <keripix@gmail.com>
2013-09-25 21:19:57 +0700

    add test for cleaner

---
+++ tests/specs/canvasServices/cleanerSpec.js
@@ -0,0 +1,21 @@
+define(function(require){
+
+  var fabric = require("fabric");
+
+  describeComponent("canvasServices", function(){
+
+    describe("Cleaning services", function(){
+
+      xit("Should remove selected objects", function(){
+
+      });
+
+      xit("Should clear the canvas", function(){
+
+      });
+
+    });
+
+  });
+
+});
