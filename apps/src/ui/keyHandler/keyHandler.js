d5f0489212c8ff84c7c670e8838069f348f1af3b
A. Akbar Hidayat <keripix@gmail.com>
2013-10-02 20:59:44 +0700

    give comments

--- apps/src/ui/keyHandler/keyHandler.js
+++ apps/src/ui/keyHandler/keyHandler.js
@@ -1,3 +1,6 @@
+/**
+ * I know how to reach to keyboard handler
+ */
 define(function(require){
 
   var defineComponent = require("flight/lib/component");
@@ -18,6 +21,10 @@ define(function(require){
       });
     };
 
+    /**
+     * Cancel any currently active painting
+     * @return {[type]} [description]
+     */
     this.cancelActivePainting = function(){
       this.trigger("cancelCurrentPainting", {
         active: "key"
