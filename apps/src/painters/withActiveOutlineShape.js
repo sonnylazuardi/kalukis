96cf35d8bd6613e8a0e4242ff52c3def71322718
A. Akbar Hidayat <keripix@gmail.com>
2013-10-04 14:54:16 +0700

    outlineShape served

--- apps/src/painters/withActiveOutlineShape.js
+++ apps/src/painters/withActiveOutlineShape.js
@@ -3,4 +3,41 @@
  */
 define(function(require){
 
+  return withActiveOutlineShape;
+
+  function withActiveOutlineShape(){
+    var activeOutlineShape;
+
+    this.after("initialize", function(){
+
+      this.on("paintWidgetClicked", function(e, data){
+        this.requestOutlineShapeInstance(data.paintWidgetId);
+      }.bind(this));
+
+      this.on("brushPropertyUpdated", function(e, data){
+
+      }.bind(this));
+
+    });
+
+    this.requestOutlineShapeInstance = function(id){
+      this.on("outlineShapeRequestResponded", function(e, data) {
+        this.off("outlineShapeRequestResponded");
+        this.setOutlineShapeInstance(data.outlineShape);
+      }.bind(this));
+
+      this.trigger("outlineShapeRequested", {
+        id: id
+      });
+    };
+
+    this.setOutlineShapeInstance = function(outlineShape) {
+      activeOutlineShape = outlineShape;
+    };
+
+    this.getActiveOutlineShape = function(){
+      return activeOutlineShape;
+    };
+  }
+
 });
---
+++ tests/specs/painters/withActiveOutlineShapeSpec.js
@@ -0,0 +1,31 @@
+define(function(require){
+
+  var fabric = require("fabric"),
+      canvas = new fabric.Canvas(),
+      CircleOutline = require("outlineShapes/circleOutline");
+
+  describeMixin("painters/withActiveOutlineShape", function(){
+
+    beforeEach(function(){
+      setupComponent();
+    });
+
+    describe("Active OutlineShape Handling", function(){
+
+      it("Should have reference to an outlineShape instance when a new one is available", function(){
+        $(".component-root").trigger("paintWidgetClicked", {
+          paintWidgetId: "circle"
+        });
+
+        $(".component-root").trigger("outlineShapeRequestResponded", {
+          outlineShape: new CircleOutline(canvas, {})
+        });
+
+        expect(this.component.getActiveOutlineShape()).toBeInstanceOf(CircleOutline);
+      });
+
+    });
+
+  });
+
+});
