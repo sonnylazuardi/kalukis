dd38d521b80b6ca995e01db5f248b053f31f60e6
A. Akbar Hidayat <keripix@gmail.com>
2013-10-04 10:59:41 +0700

    Set up active brush when activeBrushChanged is triggered

--- apps/src/painters/withActiveBrush.js
+++ apps/src/painters/withActiveBrush.js
@@ -3,4 +3,37 @@
  */
 define(function(require){
 
+  return withActiveBrush;
+
+  function withActiveBrush(){
+
+    var activeBrush;
+
+    this.after("initialize", function(){
+      this.on("activeBrushChanged", function(e, data){
+        this.requestBrushInstance(data.activeBrushId);
+      }.bind(this));
+    });
+
+    this.requestBrushInstance = function(id){
+      this.on("brushRequestResponded", function(e, data){
+        this.off("brushRequested");
+        this.setBrushInstance(data.brush);
+      }.bind(this));
+
+      this.trigger("brushRequested", {
+        id: id
+      });
+    };
+
+    this.setBrushInstance = function(brush) {
+      activeBrush = brush;
+    };
+
+    this.getActiveBrush = function() {
+      return activeBrush;
+    };
+
+  }
+
 });
--- tests/specs/painters/withActiveBrushSpec.js
+++ tests/specs/painters/withActiveBrushSpec.js
@@ -1,7 +1,8 @@
 define(function(require){
 
   var fabric = require("fabric"),
-      canvas = new fabric.Canvas();
+      canvas = new fabric.Canvas(),
+      CircleBrush = require("brushes/circle");
 
   describeMixin("painters/withActiveBrush", function(){
 
@@ -11,8 +12,15 @@ define(function(require){
 
     describe("Active Brush Handling", function(){
 
-      xit("Should have reference to a brush instance when a new brush is clicked", function(){
+      it("Should have reference to a brush instance when a new brush is clicked", function(){
+        $(".component-root").trigger("activeBrushChanged", {
+          activeBrushId: "circle"
+        });
+        $(".component-root").trigger("brushRequestResponded", {
+          brush: new CircleBrush(canvas, {})
+        });
 
+        expect(this.component.getActiveBrush()).toBeInstanceOf(CircleBrush);
       });
 
       xit("Should update the current active brush property when a property is upadted", function(){
