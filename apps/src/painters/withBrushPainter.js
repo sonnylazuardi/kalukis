0b92d14bdb8bb275a21ed1bf270d5135d42440d0
A. Akbar Hidayat <keripix@gmail.com>
2013-10-04 10:23:21 +0700

    simplify withBrushPainter responsibility

--- apps/src/painters/withBrushPainter.js
+++ apps/src/painters/withBrushPainter.js
@@ -9,144 +9,16 @@ define(function(require){
 
   function withBrushPainter(){
 
-    this.defaultAttrs({
-      /**
-       * The active brush id
-       * @type {String}
-       */
-      activeBrushId: undefined,
-
-      /**
-       * The current active brush used to paint
-       * @type {Object}
-       */
-      activeBrush: undefined,
-
-      /**
-       * A state which tells wether this mixin is
-       * requesting for an active brush instance
-       * @type {Boolean}
-       */
-      isRequestingForActiveBrushInstance: false
-    });
-
-    this.after("initialize", function(){
-
-      this.on("brushesLoaded", function(e, data){
-        this.setActiveBrushId(data.brushes[0].id);
-      });
-      
-      this.on("activeBrushChanged", function(e, data){
-        this.setActiveBrushId(data.activeBrushId);
-      }.bind(this));
-
-      this.on("brushPaintingInitted", function(e, data){
-        this.prepareBrushPainting(data.canvas, data.points, data.brush);
-      }.bind(this));
-
-      this.on("brushPropertyUpdated", function(e, data){
-        this.updateBrushProperty(data.key, data.newValue);
-      }.bind(this));
-    });
-
-    /**
-     * Set the active brush ID
-     * @param {String} id Brush ID
-     */
-    this.setActiveBrushId = function(id) {
-      this.attr.activeBrushId = id;
-
-      this.trigger("activeBrushIdUpdated", {
-        id: id
-      });
-
-      this.requestActiveBrush(id);
-    };
-
-    /**
-     * Returns the active brush ID. This could be used by
-     * the component which this mixin is composed to.
-     * 
-     * @return {String} The current brush ID
-     */
-    this.getActiveBrushId = function(){
-      return this.attr.activeBrushId;
-    };
-
-    /**
-     * Request for the active brush instance
-     * @param  {String} id The brush ID
-     */
-    this.requestActiveBrush = function(id){
-      this.on("brushRequestResponded", function(e, data){
-        this.setActiveBrush(data.brush);
-      }.bind(this));
-
-      this.attr.isRequestingForActiveBrushInstance = true;
-
-      this.trigger("brushRequested", {
-        id: id
-      });
-    };
-
-    /**
-     * Set the active brush instance
-     * @param {Object} brush The brush instance
-     */
-    this.setActiveBrush = function(brush){
-      if (this.attr.isRequestingForActiveBrushInstance) {
-        this.attr.activeBrush = brush;
-
-        this.detachFromBrushRespondEvent();
-        this.trigger("activeBrushUpdated", {
-          brush: brush
-        });
-      }
-    };
-
-    /**
-     * Returns the active brush instance. Could be used by
-     * the component which has this mixin composed to itself
-     * 
-     * @return {Object} Active brush instance
-     */
-    this.getActiveBrush = function(){
-      return this.attr.activeBrush;
-    };
-
-    /**
-     * Dont listen to the brushRequestRespondedEvent
-     */
-    this.detachFromBrushRespondEvent = function(){
-      this.off("brushRequestResponded");
-      this.attr.isRequestingForActiveBrushInstance = false;
-    };
-
-    /**
-     * Update active brush instance property
-     * @param  {String} key   The property
-     * @param  {String} value The value
-     */
-    this.updateBrushProperty = function(key, value){
-      if (this.attr.activeBrush) {
-        this.attr.activeBrush.set(key, value);
-      }
-    };
-
     /**
      * Paints the object with the active brush instance
      * @param  {Object} canvas The canvas instance
      * @param  {Array} points Points
      */
-    this.startBrushPainting = function(canvas, points, brush){
-      var usedBrush = brush || this.attr.activeBrush;
-      
-      if (usedBrush) {
-        usedBrush.drawAtPoints(points);
-        canvas.renderAll();
+    this.startBrushPainting = function(canvas, brush, points){
+      brush.drawAtPoints(points);
+      canvas.renderAll();
 
-        this.finalizePainting(usedBrush);
-      }
+      this.finalizePainting(brush);
     };
 
     /**
--- tests/specs/painters/withBrushPainterSpec.js
+++ tests/specs/painters/withBrushPainterSpec.js
@@ -10,128 +10,12 @@ define(function(require){
       setupComponent();
     });
 
-    describe("Active Brush", function(){
-
-      it("Should hold which brush is currently active", function(){
-
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        expect(this.component.attr.activeBrushId).toEqual("rect");
-      });
-
-      it("Returns the correct activeBrushID", function(){
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        expect(this.component.getActiveBrushId()).toEqual("rect");
-      });
-
-      it("Should publish event regarding active brush id changed", function(){
-        var spiedEvent = spyOnEvent(".component-root", "activeBrushIdUpdated");
-
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        expect(spiedEvent).toHaveBeenTriggeredOn(".component-root");
-        expect(spiedEvent.mostRecentCall.data.id).toEqual("rect");
-      });
-
-      it("Should set the active brush when brushesLoaded event has been triggered", function(){
-        $(".component-root").trigger("brushesLoaded", {
-          brushes: [{
-            "id": "circle",
-            "name": "Circle"
-          },
-          {
-            "id": "rect",
-            "name": "Rectangular"
-          }]
-        });
-
-        expect(this.component.attr.activeBrushId).toEqual("circle");
-      });
-
-    });
-
-    describe("Active Brush Event Management", function(){
-
-      it("Should request an instance of the active brush when the activeBrushId has been updated", function(){
-        var spiedEvent = spyOnEvent(".component-root", "brushRequested");
-
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        expect(spiedEvent).toHaveBeenTriggeredOn(".component-root");
-        expect(spiedEvent.mostRecentCall.data.id).toEqual("rect");
-      });
-
-      it("Should be listening to the brushRequestResponded event", function(){
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        $(".component-root").trigger("brushRequestResponded", {
-          brush: "rectBrush"
-        });
-
-        expect(this.component.attr.activeBrush).toEqual("rectBrush");
-      });
-
-      it("Should detach itself from brushRequestResponded event once the active brush instance has been saved", function(){
-        
-        $(".component-root").trigger("activeBrushChanged", {
-          activeBrushId: "rect"
-        });
-
-        $(".component-root").trigger("brushRequestResponded", {
-          brush: "rectBrush"
-        });
-
-        // should not update the brush instance
-        $(".component-root").trigger("brushRequestResponded", {
-          brush: "yellowBrush"
-        });
-
-        expect(this.component.attr.activeBrush).toEqual("rectBrush");
-        expect(this.component.attr.isRequestingForActiveBrushInstance).toBeFalsy();
-      });
-    });
-
-    describe("Active Brush Property Management", function(){
-
-      beforeEach(function(){
-        this.component.attr.activeBrush = new CircleBrush(canvas, {});
-        this.component.attr.activeBrushId = "circle";
-      });
-
-      it("Should update the active brush property when there is a change in the brush property", function(){
-        $(".component-root").trigger("brushPropertyUpdated", {
-          key: "width",
-          oldValue: 10,
-          newValue: 20
-        });
-
-        expect(this.component.attr.activeBrush.get("width")).toEqual(20);
-      });
-
-    });
-
     describe("Brush Painting Event Management", function(){
-
-      beforeEach(function(){
-        this.component.attr.activeBrush = new CircleBrush(canvas, {});
-        this.component.attr.activeBrushId = "circle";
-      });
 
       it("Should have executed finalizePainting after drawing the brush is finished", function(){
         var spiedEvent = spyOnEvent(".component-root", "brushPaintingFinished");
 
-        this.component.startBrushPainting(canvas, [{x: 0, y: 0}]);
+        this.component.startBrushPainting(canvas, circleBrush, [{x: 0, y: 0}]);
         expect(spiedEvent).toHaveBeenTriggeredOn(".component-root");
       });
 
