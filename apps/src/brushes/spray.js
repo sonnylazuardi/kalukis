57ec0b90c972f88cd2f1c231904c20a91a5c3701
A. Akbar Hidayat <keripix@gmail.com>
2013-10-08 22:52:05 +0700

    draw spray brush

--- apps/src/brushes/spray.js
+++ apps/src/brushes/spray.js
@@ -3,6 +3,32 @@ define(function(require){
   var fabric = require("fabric"),
       getRandomInt = fabric.util.getRandomInt;
 
+  function createSprayChunk( pointer, width, brush ) {
+    var sprayChunkPoints = [ ];
+
+    var x, y, radius = width / 2, _width;
+
+    for (var i = 0; i < brush.density; i++) {
+
+      x = fabric.util.getRandomInt(pointer.x - radius, pointer.x + radius);
+      y = fabric.util.getRandomInt(pointer.y - radius, pointer.y + radius);
+
+      if (brush.dotWidthVariance) {
+        _width = fabric.util.getRandomInt(
+          // bottom clamp width to 1
+          Math.max(1, brush.dotWidth - brush.dotWidthVariance),
+          brush.dotWidth + brush.dotWidthVariance);
+      }
+      else {
+        _width = brush.dotWidth;
+      }
+
+      sprayChunkPoints.push({ x: x, y: y, width: _width });
+    }
+
+    return sprayChunkPoints;
+  }
+
   function SprayBrush(canvas, cfg){
     this.canvas = canvas;
 
@@ -35,37 +61,41 @@ define(function(require){
     return this.cfg[key];
   };
 
-  SprayBrush.prototype.drawAt = function(point, renderAfter) {
-    if (!point.hasOwnProperty("x") || !point.hasOwnProperty("y")) {
-      throw Error("X or Y has not been defined");
-    }
-
+  SprayBrush.prototype.drawAtPoints = function( points ) {
     var originalRenderOnAddition = this.canvas.renderOnAddition;
     this.canvas.renderOnAddition = false;
 
-    this.canvas.add(new fabric.Rect({
-      width: point.width,
-      height: point.width,
-      left: point.x + 1,
-      top: point.y + 1,
-      fill: this.cfg.color,
-      hasControls: false,
-      hasRotatingPoint: false,
-      lockUniScaling: true
-    }));
+    var sprays = [],
+        point,
+        sprayChunk;
+
+    for (var i = 0, len = points.length; i < len; i++) {
+      sprayChunk = createSprayChunk(points[i], this.cfg.width, this.brush);
+
+      for (var j = 0, jlen = sprayChunk.length; j < jlen; j++) {
+        
+        sprays.push(new fabric.Rect({
+          width: sprayChunk[j].width,
+          height: sprayChunk[j].width,
+          left: sprayChunk[j].x + 1,
+          top: sprayChunk[j].y + 1,
+          fill: this.cfg.fillColor
+        }));
+      }
+    }
+
+    if (this.optimizeOverlapping) {
+      sprays = this._getOptimizedRects(sprays);
+    }
+
+    var group = new fabric.Group(sprays);
+
+    this.canvas.add(group);
+    this.canvas.fire('path:created', { path: group });
 
     this.canvas.clearContext(this.canvas.contextTop);
     this.canvas.renderOnAddition = originalRenderOnAddition;
-
-    if (renderAfter === true){
-      this.canvas.renderAll();
-    }
-  };
-
-  SprayBrush.prototype.drawAtPoints = function(points) {
-    points.forEach(function(point){
-      this.drawAt(point, false);
-    }, this);
+    this.canvas.renderAll();
   };
 
   return SprayBrush;
