227dd42213187692159d4fad819edb768f94ef74
A. Akbar Hidayat <keripix@gmail.com>
2013-10-08 22:16:13 +0700

    simplify api for hollowSquare

--- apps/src/brushes/hollowSquare.js
+++ apps/src/brushes/hollowSquare.js
@@ -32,40 +32,42 @@ define(function(require){
     return this.cfg[key];
   };
 
-  HollowSquareBrush.prototype.drawAt = function(point, renderAfter) {
-    if (!point.hasOwnProperty("x") || !point.hasOwnProperty("y")) {
-      throw Error("X or Y has not been defined");
+  HollowSquareBrush.prototype.drawObjects = function( points ) {
+    var originalRenderOnAddition = this.canvas.renderOnAddition;
+        this.canvas.renderOnAddition = false;
+
+    var rects = [],
+        width,
+        point;
+
+    for (var i = 0, len = points.length; i < len; i++) {
+      width = getRandomInt(0, this.cfg.width);
+      point = points[i];
+      rects.push(new fabric.Rect({
+        width: width,
+        height: width,
+        left: point.x,
+        top: point.y,
+        fill: null,
+        stroke: this.cfg.strokeColor,
+        hasControls: false,
+        hasRotatingPoint: false,
+        lockUniScaling: true
+      }));
     }
 
-    var originalRenderOnAddition = this.canvas.renderOnAddition;
-    this.canvas.renderOnAddition = false;
+    var group = new fabric.Group(rects);
 
-    var width = getRandomInt(0, this.cfg.width);
-
-    this.canvas.add(new fabric.Rect({
-      width: width,
-      height: width,
-      left: point.x,
-      top: point.y,
-      fill: null,
-      stroke: this.cfg.strokeColor,
-      hasControls: false,
-      hasRotatingPoint: false,
-      lockUniScaling: true
-    }));
+    this.canvas.add(group);
+    this.canvas.fire('path:created', { path: group });
 
     this.canvas.clearContext(this.canvas.contextTop);
     this.canvas.renderOnAddition = originalRenderOnAddition;
-
-    if (renderAfter === true){
-      this.canvas.renderAll();
-    }
+    this.canvas.renderAll();
   };
 
   HollowSquareBrush.prototype.drawAtPoints = function(points) {
-    points.forEach(function(point){
-      this.drawAt(point, false);
-    }, this);
+    this.drawObjects(points);
   };
 
   return HollowSquareBrush;
