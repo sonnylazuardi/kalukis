216e718bfe48d60e72f8567b0c10bd35ef731975
A. Akbar Hidayat <keripix@gmail.com>
2013-10-12 23:07:14 +0700

    it should know when its far enough

--- tests/specs/paintingUtils/brushDistanceSpec.js
+++ tests/specs/paintingUtils/brushDistanceSpec.js
@@ -10,6 +10,26 @@ define(function( require ){
         {x: 1, y: 1},
         10
       )).toBeFalsy();
+
+      expect(brushDistance.isFarEnough(
+        {x: 3,y: 3},
+        {x: 9, y: 2},
+        10
+      )).toBeFalsy();
+    });
+
+    it("Should know when it's far enought", function() {
+      expect(brushDistance.isFarEnough(
+        {x: 0,y: 0},
+        {x: 11, y: 11},
+        10
+      )).toBeTruthy();
+
+      expect(brushDistance.isFarEnough(
+        {x: 3,y: 3},
+        {x: 13, y: 13},
+        10
+      )).toBeTruthy();
     });
 
   });
