39dbc06b13b86964a60bb9c63aa5cf4459e5f47b
A. Akbar Hidayat <keripix@gmail.com>
2013-10-12 23:05:48 +0700

    should know when it is not far enough

--- tests/specs/paintingUtils/brushDistanceSpec.js
+++ tests/specs/paintingUtils/brushDistanceSpec.js
@@ -1,6 +1,16 @@
 define(function( require ){
 
+  var brushDistance = require("paintingUtils/brushDistance");
+
   describe("BrushDistanceUtils", function() {
+
+    it("Should know when it's not far enough", function() {
+      expect(brushDistance.isFarEnough(
+        {x: 0,y: 0},
+        {x: 1, y: 1},
+        10
+      )).toBeFalsy();
+    });
 
   });
 
