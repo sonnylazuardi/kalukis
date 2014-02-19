198ba465607f625e96bb404f21b865c20e2e7419
fawwazmuhammad <fawwazmuhammad@gmail.com>
2014-02-19 14:29:42 +0700

    unresolved problem always echoing "testing"

---
+++ apps/src/ui/saveWidget/saveWidget.js
@@ -0,0 +1,33 @@
+define(function(require){
+
+  var defineComponent = require('flight/lib/component'),
+      tmpl = require('text!ui/saveWidget/template.html');
+
+  return defineComponent(saveWidget);
+
+  function saveWidget() {
+
+    this.defaultAttrs({
+      saveWidgetEl: '#savewidget'
+    });
+
+    this.after('initialize', function(){
+      this.renderTemplate();
+      this.attachEventListeners();
+    });
+
+    this.renderTemplate = function(){
+      this.$node.append(tmpl);
+    };
+
+    this.attachEventListeners = function(){
+      this.on('click', function() {
+        var canvas = document.getElementById('lukis');
+        var context = canvas.getContext('2d');
+
+        window.open(canvas.toDataURL('image/png'));
+      });
+    };
+  }
+
+});
