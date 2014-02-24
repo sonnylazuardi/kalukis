f392f1db7de6c53e0d45fdfdc3245c74a9a0b0bc
fawwazmuhammad <fawwazmuhammad@gmail.com>
2014-02-24 10:57:28 +0700

    more clean code

--- apps/src/ui/FBWidget/FBWidget.js
+++ apps/src/ui/FBWidget/FBWidget.js
@@ -94,7 +94,7 @@ define(function(require){
       var fd = new FormData();
       fd.append("access_token", access_token);
       fd.append("source", blob);
-      fd.append("message", "Photo Text");
+      fd.append("message", "Sharing Picture from Kalukis Fork it on : https://github.com/sonnylazuardi/kalukis ! ");
       try {
           $.ajax({
               url: "https://graph.facebook.com/me/photos?access_token=" + access_token,
@@ -131,33 +131,6 @@ define(function(require){
             type: 'image/png'
         });
     }
-
-    this.dataURItoBlob = function(dataURI,mime) {
-      var BASE64_MARKER = ';base64,';
-      var base64Index = dataURI.indexOf(BASE64_MARKER);
-      dataURI = dataURI.substring(base64Index + BASE64_MARKER.length);
-      var byteString = window.atob(dataURI);
-      var ia = new Uint8Array(byteString.length);
-      for (var i = 0; i < byteString.length; i++) {
-          ia[i] = byteString.charCodeAt(i);
-      }
-      return new Blob([ia], { type: mime });
-    }
-
-
-    this.post_fb_multipart = function() {
-       var encodedPng=imgD.substring(start, strLength);
-       imgD = Base64Binary.decode(encodedPng);
-       var boundary = '--ThisIsTheBoundary1234567890';
-       var formData = '--' + boundary; formData += 'Content-Disposition: form-data; name="source"; filename="picMood_' + new Date().getTime();
-       formData += 'Content-Type: ' + mimeType;
-       for (var i = 0; i < imgD.length; ++i) formData += String.fromCharCode(imgD[ i ] & 0xff);
-       formData += '--' + boundary; var xhr = new XMLHttpRequest();
-       xhr.open('POST', 'https://graph.facebook.com/' + picMood.fb_album_id + '/photos?access_token=' + token + '&place=' + placeID + "&message=" + encodeURIComponent(msg), true);
-       xhr.setRequestHeader("content-type", "multipart/form-data; boundary=" + boundary);
-       xhr.sendAsBinary(formData);
-    }
-
 
 
   }
