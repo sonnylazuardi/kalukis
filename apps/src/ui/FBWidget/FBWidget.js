cb2da155f97836c344b612d5df5cf5e9937d4cc9
fawwazmuhammad <fawwazmuhammad@gmail.com>
2014-02-24 10:05:11 +0700

    semi clean code for FBWidget

--- apps/src/ui/FBWidget/FBWidget.js
+++ apps/src/ui/FBWidget/FBWidget.js
@@ -34,24 +34,103 @@ define(function(require){
         cookie     : true, // enable cookies to allow the server to access the session
         xfbml      : true  // parse XFBML
       });
-      // FB.getLoginStatus(function(response) {
-      //     alert(response);
-      // });
-      // FB.login();
+      alert("test");
+      FB.getLoginStatus(function(response) {
+          alert(response);
+      });
+      FB.login();
 
       FB.Event.subscribe('auth.authResponseChange', function(response) {
         if (response.status === 'connected') {
-            
-          // testAPI();
+            var access = FB.getAuthResponse()['accessToken'];
+            var uid;
+            FB.api('/me',function(respon){
+              uid= respon.id;
+            });
+
+            var canvas = document.getElementById('lukis');
+            var context = canvas.getContext('2d');
+            var datauri = canvas.toDataURL('image/png');
+
+            share(access,uid,datauri);
+
         } else if (response.status === 'not_authorized') {
-          FB.login();
+          MasukFb();
         } else {
-          FB.login();
+          MasukFb();
         }
       });
     };
 
     //Added by fawwaz from below from pupunzi
+
+    function MasukFb(){
+      FB.login(function(response){
+        if(response.authResponse){
+          var access = FB.getAuthResponse()['accessToken'];
+          var uid;
+          FB.api('/me',function(respon){
+            uid= respon.id;
+          });
+
+          var canvas = document.getElementById('lukis');
+          var context = canvas.getContext('2d');
+          var datauri = canvas.toDataURL('image/png');
+
+          share(access,uid,datauri);
+
+        }else{
+          console.log("Usec canceled login")
+        }
+      },{scope:publish_actions});
+    }
+
+    function share(access_token,uid,imageData){
+      try {
+          blob = dataURItoBlob(imageData);
+      } catch (e) {
+          console.log(e);
+      }
+      var fd = new FormData();
+      fd.append("access_token", access_token);
+      fd.append("source", blob);
+      fd.append("message", "Photo Text");
+      try {
+          $.ajax({
+              url: "https://graph.facebook.com/me/photos?access_token=" + access_token,
+              type: "POST",
+              data: fd,
+              processData: false,
+              contentType: false,
+              cache: false,
+              success: function (data) {
+                  console.log("success " + data);
+                  $("#poster").html("Posted Canvas Successfully");
+              },
+              error: function (shr, status, data) {
+                  console.log("error " + data + " Status " + shr.status);
+              },
+              complete: function () {
+                  console.log("Posted to facebook");
+              }
+          });
+
+      } catch (e) {
+          console.log(e);
+      }
+    }
+
+    function dataURItoBlob(dataURI) {
+        var byteString = atob(dataURI.split(',')[1]);
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        return new Blob([ab], {
+            type: 'image/png'
+        });
+    }
 
     this.dataURItoBlob = function(dataURI,mime) {
       var BASE64_MARKER = ';base64,';
